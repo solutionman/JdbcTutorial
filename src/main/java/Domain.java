0c296e864b7ece2c3599152a43ee7d42cd676ec5
lom <solutionman@inbox.ru>
2019-02-10 21:19:59 +0700

    AddressService works

--- src/main/java/Domain.java
+++ src/main/java/Domain.java
@@ -1,9 +1,28 @@
 import bl.Util;
+import entity.Address;
+import service.AddressService;
+
+import java.sql.SQLException;
 
 public class Domain {
 
     public static void main(String[] args){
         Util util = new Util();
         util.getConnection();
+
+        AddressService addressService = new AddressService();
+
+        Address address = new Address();
+        address.setId(1L);
+        address.setCountry("DC");
+        address.setCity("Gotham City");
+        address.setStreet("Arkham street");
+        address.setPostCode("12345");
+
+        try{
+            addressService.add(address);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
